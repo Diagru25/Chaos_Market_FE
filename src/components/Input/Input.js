12125d975f0646a760a8f32c70e832cc19347a47
tienpm98 <phaminhtien2498@gmail.com>
2021-07-15 10:47:35 +0700

    fix styled component input field

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,71 +1,50 @@
-// @flow 
-import * as React from 'react';
-import styled from 'styled-components';
-import './Input.css';
+// @flow
+import * as React from 'react'
+import styled from 'styled-components'
+import './Input.css'
 
 const Input = ({ placeholder }) => {
-    return (
-        // <Wrapper>
-        //     <CustomInput placeholder=" "/>
-        //     <CustomLabel>
-        //         {placeholder || 'placeholder'}
-        //     </CustomLabel>
-        // </Wrapper>
+  return (
+    <FormField>
+      <FormInput type='text' placeholder=' ' />
+      <FormLabel htmlFor='name'>{placeholder || 'Placeholder'}</FormLabel>
+    </FormField>
+  )
+}
 
-        <div className="form-field">
-            <input type="text" className="form-input" placeholder=" "/>
-            <label htmlFor="name" className="form-label">
-                {placeholder || 'Placeholder'}
-            </label>
-        </div>
-    );
-};
-
-const Wrapper = styled.div`
-    position: relative;
+const FormField = styled.div`
+  position: relative;
 `
 
-const CustomLabel = styled.label`
-    position: absolute;
-    top: 50%;
-    left: 21px;
-
-    transform: translateY(-50%);
-    transition: 0.25s ease;
-
-    user-select: none;
-    color: #999;
-    pointer-events: none;
-    font-size: 14px;
+const FormLabel = styled.label`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 21px;
+  user-select: none;
+  color: #999;
+  pointer-events: none;
+  transition: 0.25s ease;
 `
 
-const CustomInput = styled.input`
-    outline: none;
-    padding: 10px 20px;
-    border: 1px solid #eee;
-    border-radius: 4px;
-    width: 100%;
-    height: 30px;
-
-    font-size: 14px;
-
-    transition: 0.25s ease;
-
-    &:not(:placeholder-shown) + ${CustomLabel},
-    &:focus {
-        border-color: #6a5af9;
-        
-        & + ${CustomLabel} {
-            top: 0;
-            padding: 0 5px;
-            display: inline-block;
-            background-color: white;
-
-            color: #6a5af9;
-            font-size: 12px
-
-        }
-    }
-    
+const FormInput = styled.input`
+  padding: 10px 20px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  height: 30px;
+  width: 100%;
+  transition: 0.25s ease;
+  outline: none;
+  &:focus {
+    border-color: #6a5af9;
+  }
+  &:not(:placeholder-shown) + ${FormLabel}, &:focus + ${FormLabel} {
+    top: 0;
+    padding: 0 5px;
+    display: inline-block;
+    background-color: white;
+    color: #6a5af9;
+    font-size: 12px;
+  }
 `
-export default Input;
+export default Input
