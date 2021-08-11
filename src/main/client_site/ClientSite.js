a384fed10dc175ea8c72a4e62a44cd27780512d9
Diagru25 <diagru2510@gmail.com>
2021-08-11 14:27:28 +0700

    [fix folder name master]

---
+++ src/main/client_site/ClientSite.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { Switch, Route } from 'react-router-dom';
+import Header from '@src/components/header/Header';
+import Footer from '@src/components/footer/Footer';
+
+import { ClientRoutes } from '@src/routes';
+import { clientPaths } from '@src/routes/routes.constant';
+
+import CartPage from '@src/pages/cart_page/CartPage';
+
+import LoginRequired from '@src/components/login_required/LoginRequired';
+import ClientLayoutAuth from './ClientLayoutAuth';
+import ClientProfileLayout from './ClientProfileLayout';
+
+const ClientSite = ({ path }) => {
+    return (
+        <Route path={path}>
+            <Switch>
+                <ClientLayoutAuth path={clientPaths.AUTH}/>
+                <ClientProfileLayout path={clientPaths.PROFILE}/>
+                <LoginRequired
+                    exact
+                    path={clientPaths.CART}
+                    component={CartPage}
+                    redirectLink={clientPaths.AUTH_LOGIN}
+                />
+                <div>
+                    <Header />
+                    <Switch>
+                        {
+                            ClientRoutes.map(
+                                (route, index) => <Route
+                                    key={index}
+                                    exact={route.exact}
+                                    path={route.path}
+                                    component={route.component} />
+                            )
+                        }
+                    </Switch>
+                    <Footer />
+                </div>
+            </Switch>
+        </Route>
+    );
+};
+
+export default ClientSite;
